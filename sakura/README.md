5efe4267887e2527a8cac058e7ff43507131c430
dingchaofa <571797089@qq.com>
2017-09-19 19:18:56 +0800

    小樱

--- sakura/README.md
+++ sakura/README.md
@@ -1,17 +1,47 @@
 ## 小樱 Let's go
 
-#### 说明
+http://www.nicovideo.jp/watch/sm19168559
 
-###### 库类
+Some scenery, once to see into the heart,even in the moment, is eternal
+
+### 说明
+
+#### 库类
 1. stats.js
-three.js做动画帧数统计  http://github.com/mrdoob/stats.js
+- three.js做动画帧数统计  http://github.com/mrdoob/stats.js
 
 2. mmdparser.js
 - mmd解析器，解析MMD二进制数组或者字符串为对象
 - MikuMikuDance（MMD）开发 MikuMikuDance是日本人樋口优所开发，将VOCALOID2的初音未来等角色制作3D模组的免费软件。 简称为MMD。   
-http://tieba.baidu.com/p/2171800749  
-http://javascript.ruanyifeng.com/stdlib/arraybuffer.html
+- http://tieba.baidu.com/p/2171800749  
+- http://javascript.ruanyifeng.com/stdlib/arraybuffer.html
 
 3.ammon.js
 - 一个用 C++ 语言编写的物理库，可以看作是 WebGL 上的 Bullet 物理引擎端口。Emscripten 会直接将项目的源码从 C++ 转换成 Javascript ，无需人工重写，功能基本与原始 Bullet 相同。  
-https://github.com/kripken/ammo.js/
+- https://github.com/kripken/ammo.js/
+
+4. TGALoader.js
+- 用来加载一个 .tga格式的纹理（texture）的类。  
+- http://techbrood.com/threejs/docs/#参考手册/纹理(Textures)/数据纹理(DataTexture)
+
+5. MMDLoader.js  
+- 加载mmd数据
+
+6. OutlineEffect.js
+- 生成模型轮廓
+https://stemkoski.github.io/Three.js/Outline.html
+
+7. CCDIKSolver.js
+- 利用CCD算法，解决目标逆运动问题  
+- https://sites.google.com/site/auraliusproject/ccd-algorithm
+
+8. MMDPhysics.js
+- 物体运动轨迹仿真
+- https://chandlerprall.github.io/Physijs/
+
+9. Detector.js
+- 判断浏览器是否支持webGL
+- https://stackoverflow.com/questions/16504431/detect-webgl-support-should-we-use-detector-js-or-system-min-js
+
+#### MMD动作教程
+- http://www.bilibili.com/video/av2581355/?from=search&seid=7079999244908259487
--- sakura/js/main.js
+++ sakura/js/main.js
@@ -1,1 +1,129 @@
 var container,stats
+
+var mesh, camera, scene, renderer, effect
+var helper
+
+var ready = false
+
+var mouseX = 0, mouseY = 0
+
+var windowHalfX = window.innerWidth/2,
+    windowHalfY = window.innerHeight/2
+
+var clock = new THREE.Clock() //跟踪时间的对象
+
+init();
+animate();
+
+function init() {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+
+  camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,1,2000)
+
+  scene = new THREE.Scene()
+  scene.add(new THREE.PolarGridHelper(30,10)) //定义一个半径是30，10条射线的极面网格，网格是一组二维的面
+
+  var ambient = new THREE.AmbientLight(0x666666) //环境光
+  scene.add(ambient)
+
+  var directionalLight = new THREE.DirectionalLight(0x887766) //平行光
+  directionalLight.position.set(-1,1,1).normalize() //设置光的方向
+  scene.add(directionalLight)
+
+  renderer = new THREE.WebGLRenderer({antialias:true}) //antialias抗锯齿
+  renderer.setPixelRatio(window.devicePixelRatio) //设备像素比
+  renderer.setSize(window.innerWidth,window.innerHeight) //渲染大小
+  renderer.setClearColor(new THREE.Color(0xffffff)) //设置清除的颜色
+  container.appendChild((renderer.domElement)) //把canvas添加到DOM上
+
+  effect = new THREE.OutlineEffect(renderer) //生成目标轮廓效果
+
+  var onProgress = function (xhr) {
+    if(xhr.lengthComputable){
+      var percentComplete = xhr.loaded/xhr.total*100
+      console.log(Math.round(percentComplete)+'% downloaded')
+    }
+  }
+
+  var onError = function (xhr) {
+
+  }
+
+  var modelFile = 'models/mmd/miku/miku_v2.pmd'; //一种三维模型
+  var vmdFiles = [ 'models/mmd/vmds/wavefile_v2.vmd' ]; //模型动作
+  var cameraFiles = [ 'models/mmd/vmds/wavefile_camera.vmd' ]; //相机动作
+  var audioFile = 'models/mmd/audios/wavefile_short.mp3';
+  var audioParams = { delayTime: 160 * 1 / 30 };
+
+  helper = new THREE.MMDHelper() //MMDLoader.js
+
+  var loader = new THREE.MMDLoader()
+
+  loader.load(modelFile,vmdFiles,function (object) {
+    mesh = object
+
+    helper.add(mesh)
+    helper.setAnimation(mesh)
+    helper.setPhysics(mesh)
+
+    loader.loadVmds(cameraFiles,function (vmd) {
+      helper.setCamera(camera)
+
+      loader.pourVmdIntoCamera(camera,vmd)
+
+      helper.setCameraAnimation(camera)
+
+      loader.loadAudio(audioFile,function (audio,listener) {
+        listener.position.z = 1
+        helper.setAudio(audio,listener,audioParams)
+
+        helper.unifyAnimationDuration()
+
+        scene.add(audio)
+        scene.add(listener)
+        scene.add(mesh)
+
+        ready = true
+      },onProgress,onError)
+    },onProgress,onError)
+  },onProgress,onError)
+
+  document.addEventListener('mousemove',onDocumentMouseMove)
+
+  window.addEventListener('resize',onWindowResize)
+}
+
+function onDocumentMouseMove() {
+  mouseX = ( event.clientX - windowHalfX ) / 2;
+  mouseY = ( event.clientY - windowHalfY ) / 2;
+}
+
+function onWindowResize() {
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  effect.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+  render();
+
+}
+
+function render() {
+
+  if ( ready ) {
+
+    helper.animate( clock.getDelta() );
+
+  }
+
+  effect.render( scene, camera );
+
+}
